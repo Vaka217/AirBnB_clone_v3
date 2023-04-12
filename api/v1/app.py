9e030e2d801fa2f78043759b839b5c904f12feac
Vaka217 <gonzalocabaleiro12@gmail.com>
2023-04-12 07:58:59 -0700

    Start the API

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from os import getenv
+from api.v1.views import app_views
+from models import storage
+from flask import Flask
+
+app = Flask(__name__)
+
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    h, p = '0.0.0.0', '5000'
+    if getenv('HBNB_API_HOST'):
+        h = getenv('HBNB_API_HOST')
+    if getenv('HBNB_API_PORT'):
+        p = getenv('HBNB_API_PORT')
+    app.run(host=h, port=p, threaded=True, debug=True)
